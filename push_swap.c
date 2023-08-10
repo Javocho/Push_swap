f0bf54608c47760f0d39d0f7e3be264d53acff81
Francisco Javier Costa Ferrer-Vidal <fcosta-f@car14s6.42barcelona.com>
2023-08-10 21:21:42 +0200

    modifying struct and method to insert nums to stack

--- push_swap.c
+++ push_swap.c
@@ -3,25 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fcosta-f <fcosta-f@student.42barcelona.    +#+  +:+       +#+        */
+/*   By: fcosta-f <fcosta-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 18:34:46 by fcosta-f          #+#    #+#             */
-/*   Updated: 2023/08/10 01:19:54 by fcosta-f         ###   ########.fr       */
+/*   Updated: 2023/08/10 20:08:04 by fcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	is_num(char c)
+int	ft_is_num(char c)
 {
 	return (c >= '0' && c <= '9');
 }
 
-int repeat_checker(int argc, char **argv)
+int	repeat_checker(int argc, char **argv)
 {
 	int	i;
 	int	j;
-	int n;
+	int	n;
 
 	i = 0;
 	j = 0;
@@ -44,18 +44,20 @@ int	ft_checker(int argc, char **argv)
 	int	i;
 	int	j;
 
-	i = 0;
+	i = 1;
 	j = 0;
 	while (i < argc)
 	{
-		if (!is_num(argv[i][j]))
+		while (argv[i][j] != '\0')
 		{
-			write(1, "error", 5);
-			return (0);
+			if (!ft_is_num(argv[i][j]))
+			{
+				write(1, "error", 5);
+				return (0);
+			}
+			j++;
 		}
-		if (argv[i][j] == '\0')
-			i++;
-		j++;
+		i++;
 	}
 	if (!repeat_checker(argc, argv))
 		return (0);
@@ -64,7 +66,30 @@ int	ft_checker(int argc, char **argv)
 
 void	insert_stack(int argc, char **argv, t_stack **stk)
 {
-	
+	t_node	*node;
+	int		i;
+
+	i = 2;
+	node = malloc(sizeof(t_node *) * 1);
+	node->content = ft_atoi(argv[1]);
+	node->index = 0;
+	(*stk)->size = argc - 1;
+	(*stk)->first = node;
+	while (argc < i)
+	{
+		node->next = malloc(sizeof(t_node *) * 1);
+		node = node->next;
+		node->index = i - 1;
+		(*stk)->last = node;
+	}
+}
+
+void	free_stack(t_stack **stk)
+{
+	t_node node;
+
+	node = (*stk)->first;
+	while (node != (*skt)->)
 }
 
 int	main(int argc, char **argv)
@@ -76,19 +101,12 @@ int	main(int argc, char **argv)
 		return (-1);
 	if (!ft_checker(argc, argv))
 		return (-1);
-	a = ft_lstnew(NULL);
 	insert_stack(argc, argv, &a);
-	while(a->next != NULL)
-	{
-		printf("%d\n", a->content);
-		a = a->next;
-	}
-	while (a)
-    {
-        t_stack *temp = a->next;
-        free(a);
-        a = temp;
-    }
+	// while(a->next != NULL)
+	// {
+	// 	printf("%d\n", a->content);
+	// 	a = a->next;
+	// }
+	//free_stack(&a);
 	a = NULL;
-	//free(b);
 }
--- push_swap.h
+++ push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fcosta-f <fcosta-f@student.42barcelona.    +#+  +:+       +#+        */
+/*   By: fcosta-f <fcosta-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 18:33:54 by fcosta-f          #+#    #+#             */
-/*   Updated: 2023/08/10 01:19:26 by fcosta-f         ###   ########.fr       */
+/*   Updated: 2023/08/10 20:14:08 by fcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,18 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-typedef struct s_node
-{
-	int	content;
-	struct s_node *next;
-	struct s_node *ant;
-	int	index;
-}	t_node;
-
 typedef struct s_stack
 {
-	t_node	*first;
-	t_node	*last;
-	int		size;
+	int				content;
+	struct s_stack	*next;
+	int				index;
 }	t_stack;
+
+// typedef struct s_stack
+// {
+// 	t_node	*first;
+// 	t_node	*last;
+// 	int		size;
+// }	t_stack;
 
 #endif
